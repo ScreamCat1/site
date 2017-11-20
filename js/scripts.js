130a02205feceb6c5adfe1865b6989e3506997bd
ScreamCat1 <maksympushkaryov@gmail.com>
2017-11-21 01:57:10 +0200

    added mobile button and action to buttons

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('.header-mobile').click(function(){
+    $('.present-left-side').toggleClass('present-left-side__active');
+    $(this).toggleClass('header-mobile__active');
+  })
+});
