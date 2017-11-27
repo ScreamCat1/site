b2956d717ec74136b2db10f625f4b15764e42fa6
Maksym Pushkarov <Maksym_Pushkarov@epam.com>
2017-11-27 19:38:06 +0200

    added basket toggle

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,8 @@ $(function(){
     $('.present-left-side').toggleClass('present-left-side__active');
     $(this).toggleClass('header-mobile__active');
   })
+
+  $('.header-basket').click(function(){
+    $('.basket-wrap').toggleClass('basket-wrap__active');
+  });
 });
